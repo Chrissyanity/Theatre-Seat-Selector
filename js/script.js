4a6e5cec1ef6020ed5f0f9835be58b8407c69b2e
Brad Cupp <bradjcupp@gmail.com>
2017-02-15 10:46:06 -0500

    changed seat colors

--- js/script.js
+++ js/script.js
@@ -1,50 +1,23 @@
 $(function() {
 
-$('.reclinerSeat').on('mouseover', function(){
-  $(this).addClass('hover')
-}).on('mouseleave', function(){
-  $(this).removeClass('hover')
-});
+    $('.reclinerSeat').on('mouseover', function() {
+        $(this).addClass('hover')
+    }).on('mouseleave', function() {
+        $(this).removeClass('hover')
+    });
 
-$('.regularSeat').on('mouseover', function(){
-  $(this).addClass('hover')
-}).on('mouseleave', function(){
-  $(this).removeClass('hover')
-});
-
-
+    $('.regularSeat').on('mouseover', function() {
+        $(this).addClass('hover')
+    }).on('mouseleave', function() {
+        $(this).removeClass('hover')
+    });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//toggle color on selected seats so users can unselect seats on second click.
+    $('.reclinerSeat').click(function() {
+        $(this).toggleClass("selectedSeat");
+    });
 
 
 
